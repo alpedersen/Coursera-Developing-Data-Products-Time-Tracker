abc9bb8f942a73a6f1b7876a568e461bda42bcdd
alpedersen <alpedersen.1441@gmail.com>
2016-07-16 13:39:28 -0500

    Add Files
    Adding Server.R and ui.R for time tracker shiny app

---
+++ ui.R
@@ -0,0 +1,24 @@
+library(shiny)
+
+shinyUI(pageWithSidebar(
+        headerPanel("Time Tracker"),
+
+                sidebarPanel(
+                        h3('Your Input'),
+                        numericInput('id1', 'Start Time (ie. 15 min=0.25, 0=12AM, 23=11PM)', 0, min = 0, max = 23.75, step = 0.25),
+                        numericInput('id2', 'End Time (ie. 15 min=0.25, 0=12AM, 23=11PM)', 0, min = 0, max = 23.75, step = 0.25),
+                        dateInput("date", "Date: "),
+                        submitButton('Submit')
+                        ),
+                mainPanel(
+                        h3('Time Worked'),
+                        h4('Date'),
+                        verbatimTextOutput("odate"),
+                        h4('Start Time'),
+                        verbatimTextOutput("oid1"),
+                        h4('End Time'),
+                        verbatimTextOutput("oid2")
+                        )
+        )
+)
+
